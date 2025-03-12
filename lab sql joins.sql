2f6df38cf3ae0eb08d709f916eb568401fc4bb9d
María del Rocío Romo García <bloomblaze7896@gmail.com>
2025-03-12 02:04:10 +0100

    Ping

--- lab sql joins.sql
+++ lab sql joins.sql
@@ -1,34 +1,41 @@
+USE sakila;
 -- 1. Enumere la cantidad de películas por categoría.
 
-SELECT COUNT(film_id) FROM film_category;
+SELECT name as category_name, COUNT(*) as num_films FROM category 
+JOIN film_category USING (category_id) GROUP BY name ORDER BY num_films DESC;
 
 -- 2. Recupere el ID de la tienda, la ciudad y el país de cada tienda.
 
-SELECT store_id FROM store
-UNION ALL
-SELECT city_id FROM city
-UNION ALL
-SELECT country_id FROM country;
+SELECT s.store_id, c.city, co.country FROM store s 
+JOIN address a ON s.address_id = a.address_id 
+JOIN city c ON a.city_id = c.city_id
+JOIN country co ON c.country_id = co.country_id;
 
 -- 3. Calcule los ingresos totales generados por cada tienda en dólares.
 
-SELECT SUM(amount) AS ventas_tot FROM payment;
+SELECT store.store_id, SUM(payment.amount) AS total FROM store
+JOIN staff ON store.store_id = staff.store_id
+JOIN payment ON staff.staff_id = payment.staff_id
+GROUP BY store.store_id;
 
 -- 4. Determine el tiempo de exhibición promedio de las películas para cada categoría.
 
-SELECT AVG(length) AS media_tiempo_exibicion FROM film;
+SELECT category.name, ROUND(AVG(film.length), 2) AS average_running_time
+FROM category JOIN film_category ON category.category_id = film_category.category_id
+JOIN film ON film_category.film_id = film.film_id GROUP BY category.name;
+
 
 -- BONUS: 
 -- 5. Identifique las categorías de películas con el tiempo de exhibición promedio más largo.
 
 -- 6. Muestre las 10 películas más alquiladas en orden descendente.
 
-SELECT length, 
-	AVG(length) AS promedio_duracion,
-	COUNT(*) AS frecuencia
+SELECT film.title, COUNT(rental.rental_id) AS rental_count
 FROM film
-GROUP BY length
-ORDER BY frecuencia DESC
+JOIN inventory ON film.film_id = inventory.film_id
+JOIN rental ON inventory.inventory_id = rental.inventory_id
+GROUP BY film.film_id
+ORDER BY rental_count DESC
 LIMIT 10;
 
 -- 7. Determine si "Academy Dinosaur" se puede alquilar en la Tienda 1.
